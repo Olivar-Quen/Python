18486ef65aa2832625133d9a436af53f8755957a
Olivar-Quen <sylvain.m.wagner@gmail.com>
2025-03-25 22:40:38 +0100

    Update conway.py

--- conway.py
+++ conway.py
@@ -3,8 +3,8 @@ import random
 rows = ""
 cols = ""
 
-rows = int(input("Nombre de lignes : "))
-cols = int(input("Nombre de colonnes: "))
+rows = int(input("Nombre de lignes : ")) # Pour parcourir les lignes on utilise la lettre i
+cols = int(input("Nombre de colonnes: "))# Pour parcourir les colonnes on utilise la lettre j
 
 
 grid = [[random.choice([0 , 1]) for _ in range(cols)] for _ in range(rows)]
