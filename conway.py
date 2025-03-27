d1f0fbd7054d082e3613741202d5dd82c4be7d95
Olivar-Quen <sylvain.m.wagner@gmail.com>
2025-03-27 18:01:46 +0100

    Update conway.py

--- conway.py
+++ conway.py
@@ -3,11 +3,26 @@ import random
 rows = ""
 cols = ""
 
+
+
+#Initialisation suivant les données rentrées par l'utilisateur 
+
 rows = int(input("Nombre de lignes : ")) # Pour parcourir les lignes on utilise la lettre i
 cols = int(input("Nombre de colonnes: "))# Pour parcourir les colonnes on utilise la lettre j
 
 
-grid = [[random.choice([0 , 1]) for _ in range(cols)] for _ in range(rows)]
+state = [[random.choice([0 , 1]) for x in range(cols)] for y in range(rows)]
+temp[x][y] = 0
 
-for row in grid :
-    print(row)
+
+
+#Définition des 3 règles 
+
+def calcul 
+	for x range(cols)
+		for y range(rows)
+			nb_voisins = calcul_voisins(x,y) 
+
+			#règle 1 : cellule a strictement 3 cellules vivantes, elle reste en vie
+
+			if nb_voisins == 3 
